df3ede28fac76afd2dc422bbecf73c0baba843b7
Edouard Burel <bureledo@gmail.com>
2026-07-03 11:22:39 +0200

    GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Si vous déployez sur GitHub Pages, remplacez base par '/<nom-du-repo>/'
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  base: "/nez-a-nez/", // ← your repo name, between two slashes
+});
