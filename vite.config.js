50f7a69ddae476b6c048aafb7f3a0779e8fc181f
Edouard Burel <bureledo@gmail.com>
2026-07-04 22:26:13 +0200

    Domaine personnalisé

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nez-a-nez/", // ← your repo name, between two slashes
+  base: '/', // ← your repo name, between two slashes
 });
